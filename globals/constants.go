b49273f18a9f7a7c35d35d1fae71358318fb817f
mattishere <69674861+mattishere@users.noreply.github.com>
2024-01-17 21:25:06 +0100

    Fixed an issue

--- globals/constants.go
+++ globals/constants.go
@@ -2,7 +2,6 @@ package globals
 
 var (
 	Empty         = ""
-	AppID         = "825639630734557716"
 	DiscordInvite = "https://discord.gg/KS5fqf7WJ8"
 	BotInvite     = "https://discord.com/oauth2/authorize?client_id=1189356164132130956&permissions=16384&scope=bot"
 	TopGGURL      = "https://top.gg/bot/1189356164132130956"
